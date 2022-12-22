48d6253529c97fcab21f9b1776c64b462efb322f
Jens Carlén <jens.carlen@offerta.se>
2022-12-22 22:15:43 +0100

    Day1

--- src/day1/Program.cs
+++ src/day1/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using System.Reflection;
+using System.Text.Json;
+using Day1.Lib;
+
+internal class Program
 {
-    private static void Main(string[] args)
+
+
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var input = await GetFileContents();
+        Console.WriteLine($"Day1, task1: {JsonSerializer.Serialize(Puzzle.FindHeaviestElf(input))}");
+        Console.WriteLine($"Day1, task2: {JsonSerializer.Serialize(Puzzle.FindTopThree(input))}");
+    }
+
+    private static Task<string> GetFileContents()
+    {
+        var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        return File.ReadAllTextAsync(Path.Combine(outPutDirectory, "elfs.txt"));
     }
 }
